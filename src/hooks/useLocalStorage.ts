84dc7478a95569003ec5f5b9469242a7e64f5c5e
HaYeong <hy78733@gmail.com>
2023-10-08 11:28:11 +0900

    ✨ feat: useLocalStorage - 로컬 스토리지에서 특정 키에 대한 CRUD

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,41 @@
+import { useCallback } from 'react';
+
+export default function useLocalStorage<T>(key: string) {
+  const save = useCallback(
+    (json: T) => {
+      localStorage.setItem(key, JSON.stringify(json));
+    },
+    [key]
+  );
+
+  const update = useCallback(
+    (json: Partial<T>) => {
+      const item = localStorage.getItem(key);
+      try {
+        if (!item) throw Error();
+
+        const parsed = JSON.parse(item);
+        save({ ...parsed, ...json });
+      } catch {
+        console.error(`'${key}' 키값에 해당하는 localStorage 값이 없습니다`);
+      }
+    },
+    [key, save]
+  );
+
+  const destroy = useCallback(() => {
+    localStorage.removeItem(key);
+  }, [key]);
+
+  const get = useCallback(() => {
+    const item = localStorage.getItem(key);
+    try {
+      if (!item) throw Error();
+      return JSON.parse(item) as T;
+    } catch {
+      return null;
+    }
+  }, [key]);
+
+  return { save, update, destroy, get };
+}

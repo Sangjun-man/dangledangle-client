337b9bad414fcec141b4e09f445a80db68b8e286
HaYeong <hy78733@gmail.com>
2023-10-12 19:41:30 +0900

    ✨ feat: useRouteGuard - 브라우저 뒤로가기, 새로고침 방지 (form 페이지에서 사용)

---
+++ src/hooks/useRouteGuard.tsx
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useState } from 'react';
+import useDialog from './useDialog';
+import { useRouter } from 'next/navigation';
+
+// 브라우저 뒤로가기 막기
+export default function useRouteGuard(callback?: () => unknown) {
+  const [routable, setRoutable] = useState(false);
+  const { dialogOn, dialogOff } = useDialog();
+  const router = useRouter();
+  const defaultCallback = useCallback(() => {
+    dialogOn({
+      message: '앗! 이대로 나가면 입력하신 정보가 모두 사라져요!',
+      confirm: {
+        text: '나가기',
+        variant: 'filled',
+        onClick: () => {
+          setRoutable(true);
+          dialogOff();
+          router.back();
+        }
+      },
+      close: {}
+    });
+  }, [dialogOff, dialogOn, router]);
+
+  const onBeforeunload = useCallback((event: BeforeUnloadEvent) => {
+    event.preventDefault();
+    event.returnValue = '';
+  }, []);
+
+  const onPopstate = useCallback(() => {
+    history.pushState('Route guard', '');
+    callback ? callback() : defaultCallback();
+  }, [callback, defaultCallback]);
+
+  useEffect(() => {
+    if (!routable) {
+      // 브라우저 새로고침 또는 닫기 방지
+      window.onbeforeunload = onBeforeunload;
+      // 브라우저 뒤로가기 방지
+      window.onpopstate = onPopstate;
+    } else {
+      window.onbeforeunload = null;
+      window.onpopstate = null;
+      history.back(); // 'Route guard' state 제거
+    }
+  }, [onBeforeunload, onPopstate, routable]);
+
+  useEffect(() => {
+    history.pushState('Route guard', '');
+    return () => {
+      window.onpopstate = null;
+    };
+  }, []);
+
+  return { setRoutable };
+}
